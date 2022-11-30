a835961aa4bbbc856eda564c3e681294eb8f1283
Sebastiaan <tientjieit@gmail.com>
2022-11-30 22:23:55 +0100

    day2

--- 2015/Python/day2.py
+++ 2015/Python/day2.py
@@ -1,16 +1,28 @@
+import math
+from statistics import median
 from utils import read_file
 from typing import List
 
 def part1(input: List[str]) -> int:
+    total_surface = 0
     for line in input:
         l,w,h = [int(num) for num in line.split("x")]
-    return 0
+        surface = 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)
+        total_surface += surface 
+
+    return total_surface
 
 def part2(input: List[str]) -> int:
-    return 0
+    total_ribbon = 0
+    for line in input:
+        l,w,h = [int(num) for num in line.split("x")]
+        ribbon = l*w*h + 2*min(l,w,h) + 2* median([l,w,h])
+        total_ribbon += ribbon 
+
+    return total_ribbon
 
 def main():
-    puzzle_input = read_file("day2.txt")
+    puzzle_input = read_file("day2")
     print(part1(puzzle_input))
     print(part2(puzzle_input))
 
