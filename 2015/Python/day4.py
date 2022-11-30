5e79f6f96ae633000586ee3fb97a4b2c611c8128
Sebastiaan <tientjieit@gmail.com>
2022-11-30 23:56:20 +0100

    day4

--- 2015/Python/day4.py
+++ 2015/Python/day4.py
@@ -1,11 +1,30 @@
 from utils import read_file
 from typing import List
+import hashlib
 
 def part1(input: List[str]) -> int:
-    return 0
+    prefix = input[0]
+    i = 0
+    while True:
+
+        hash_result = hashlib.md5((prefix + str(i)).encode())
+        digested_hash = list(hash_result.digest())
+        if digested_hash[0] == 0 and digested_hash[1] == 0 and digested_hash[2] <= 0X0F:
+            return i
+        
+        i += 1
 
 def part2(input: List[str]) -> int:
-    return 0
+    prefix = input[0]
+    i = 0
+    while True:
+
+        hash_result = hashlib.md5((prefix + str(i)).encode())
+        digested_hash = list(hash_result.digest())
+        if digested_hash[0] == 0 and digested_hash[1] == 0 and digested_hash[2] == 0:
+            return i
+        
+        i += 1
 
 def main():
     puzzle_input = read_file("day4")
