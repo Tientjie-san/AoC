1537ae23fe071b6beac581395619c24685f8e64b
Sebastiaan <tientjieit@gmail.com>
2022-12-01 00:45:03 +0100

    2022 preperation

---
+++ 2022/Python/day1.py
@@ -0,0 +1,17 @@
+from utils import read_file
+from typing import List
+
+def part1(input: List[str]) -> int:
+    return 0
+
+def part2(input: List[str]) -> int:
+    return 0
+
+def main():
+    puzzle_input = read_file("day1")
+    print(part1(puzzle_input))
+    print(part2(puzzle_input))
+
+
+if __name__ == "__main__":
+    main()
