884df15732228aac20aaed3a5f3b7d546d57ade0
TientjieIT <tientjieit@gmail.com>
2022-12-01 10:29:53 +0100

    day1

--- 2022/Python/day1.py
+++ 2022/Python/day1.py
@@ -2,10 +2,28 @@ from utils import read_file
 from typing import List
 
 def part1(input: List[str]) -> int:
-    return 0
+    totals = []
+    current_sum = 0
+    for str_num in input:
+        if str_num == "":
+            totals.append(current_sum)
+            current_sum = 0
+        else:
+            current_sum += int(str_num)
+
+    return max(totals)
 
 def part2(input: List[str]) -> int:
-    return 0
+    totals = []
+    current_sum = 0
+    for str_num in input:
+        if str_num == "":
+            totals.append(current_sum)
+            current_sum = 0
+        else:
+            current_sum += int(str_num)
+    sorted_totals = sorted(totals, reverse=True)
+    return sum(sorted_totals[0:3])
 
 def main():
     puzzle_input = read_file("day1")
